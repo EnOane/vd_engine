cc97f62d88d920fea3f3b628cffdc81e347aaf13
Kovalev Pavel <pavelkovalev1605@gmail.com>
2025-03-19 13:01:54 +0700

    feat(s3): Implement S3 upload functionality and add unit tests
    - Create `uploadToS3` function for uploading data streams to S3.
    - Implement `Execute` function to manage video download and streaming, including sending file names and chunks via gRPC.
    - Add `sendFilename` and `sendChunks` helper functions to facilitate gRPC streaming.
    - Write unit test for `uploadToS3` to ensure correct functionality with S3.
    - Set up test environment by loading configuration and establishing S3 connection in tests.

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,14 +1,131 @@
 package service
 
 import (
+	"context"
 	dl "github.com/EnOane/cli_downloader/pkg/downloader"
+	tgpb "github.com/EnOane/vd_engine/generated"
+	"github.com/EnOane/vd_engine/internal/infr/s3"
+	"github.com/EnOane/vd_engine/internal/util"
 	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
 	"io"
 	"net/url"
 	"os"
+	"path/filepath"
 )
 
-func Execute(uri *url.URL) (<-chan []byte, string, error) {
+// 50 мб лимит отправки видео в ТГ, как быть с остальными клиентами?
+// отправлять ссылку на файл в s3
+// если формат mp4 все-таки не поддерживается - отправлять как документ (только тг)
+
+func Execute(
+	request *tgpb.DownloadVideoStreamRequest,
+	stream grpc.ServerStreamingServer[tgpb.DownloadVideoStreamResponse],
+) error {
+	// проверка url
+	uri, err := url.Parse(request.Url)
+	if err != nil {
+		log.Error().Err(err).Msg("error")
+		return err
+	}
+
+	in, filenamePath, err := DownloadVideoStream(uri)
+	if err != nil {
+		log.Error().Err(err).Msg("error")
+		return err
+	}
+
+	// первое сообщение в потоке - имя файла
+	err = sendFilename(filenamePath, stream)
+	if err != nil {
+		log.Error().Err(err).Msg("error")
+		return err
+	}
+
+	// второе сообщение в потоке - chunk файла
+	out, err := sendChunks(in, stream)
+	if err != nil {
+		log.Error().Err(err).Msg("error")
+		return err
+	}
+
+	// конвейрная загрузка в s3
+	err = uploadToS3(out, filenamePath)
+	if err != nil {
+		log.Error().Err(err).Msg("error")
+		return err
+	}
+
+	log.Info().Msg("video was uploaded to s3")
+
+	return nil
+}
+
+// sendFilename отправка имени файла
+func sendFilename(
+	filenamePath string,
+	stream grpc.ServerStreamingServer[tgpb.DownloadVideoStreamResponse],
+) error {
+	return stream.Send(&tgpb.DownloadVideoStreamResponse{
+		Data: &tgpb.DownloadVideoStreamResponse_Filename{
+			Filename: filepath.Base(filenamePath),
+		},
+	})
+}
+
+// sendChunks отправка чанками в стрим grpc
+func sendChunks(in <-chan []byte, stream grpc.ServerStreamingServer[tgpb.DownloadVideoStreamResponse]) (chan []byte, error) {
+	out := make(chan []byte)
+
+	go func() {
+		defer close(out)
+
+		for data := range in {
+			err := stream.Send(&tgpb.DownloadVideoStreamResponse{
+				Data: &tgpb.DownloadVideoStreamResponse_Chunk{
+					Chunk: data,
+				},
+			})
+			if err != nil {
+				log.Error().Err(err).Msg("error")
+				return
+			}
+
+			out <- data
+		}
+	}()
+
+	return out, nil
+}
+
+// uploadToS3 отправка чанками в стрим grpc
+func uploadToS3(in <-chan []byte, filename string) error {
+	fileName := filepath.Base(filename)
+
+	reader := util.NewChannelReader(in)
+
+	_, err := s3.UploadFile(context.TODO(), fileName, reader)
+	if err != nil {
+		log.Error().Err(err).Msg("error")
+		return err
+	}
+
+	return nil
+}
+
+// DownloadVideoStream создание потока из stdout
+func DownloadVideoStream(uri *url.URL) (<-chan []byte, string, error) {
+	// скачивание файла в папку temp
+	ch, fname, err := dl.DownloadStreamVideo(uri)
+	if err != nil {
+		log.Error().Err(err).Msg("error")
+		return nil, "", err
+	}
+	return ch, fname, nil
+}
+
+// DownloadVideoFile создание потока из чтения файла
+func DownloadVideoFile(uri *url.URL) (<-chan []byte, string, error) {
 	rootPath, err := os.Getwd()
 	if err != nil {
 		log.Error().Err(err).Msg("error")
